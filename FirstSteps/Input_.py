59c24d51742329b1c50736e09a88c649b6b03276
Svitlana Pushchalo <svitlana8@gmail.com>
2021-08-28 17:00:41 +0300

    Add new project

--- FirstSteps/Input_.py
+++ FirstSteps/Input_.py
@@ -1,24 +1,36 @@
-# (1)
+# ()
 # print("Здравствуй,", input())
 #
-# (2)
+# ()
 # surname = input()
 # name = input()
 # text = "Новый зарегистрированный пользователь:"
 # print(text, name, surname)
 #
-# (3)
+# ()
 # day = input()
 # month = input()
 # year = input()
 # info = "Дата рождения пользователя:"
 # print(info, year, "-", month, "-", day)
 #
-# (4)
+# ()
 # word = input()
 # print(word, "действительно есть в нашем словаре!")
 #
-# (5)
-author = input()
-book = input()
-print(author, "- автор бестселлера %s" % book, "- выпустил новую книгу! Спешите приобрести ее в магазине Питонист!")
+# ()
+# author = input("Введіть автора: ")
+# book = input("Введіть назву книги: ")
+# print(author, "- автор бестселлера %s" % book, "- выпустил новую книгу! Спешите приобрести ее в магазине Питонист!")
+
+# ()
+# 11 22 33
+# a, b, c = map(int, input().split())
+# print(a, b, c)   # 11 22 33
+
+# ()
+print(1, 2, 3, sep=', ')   # 1, 2, 3
+print(4, 5, sep="???", end=" !!! ")
+print(6, 7)
+# 1, 2, 3
+# 4???5 !!! 6 7
