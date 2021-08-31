ee023e115a27fbf33815ba8b25d4ec13ab7bb397
Svitlana Pushchalo <svitlana8@gmail.com>
2021-08-31 15:31:49 +0300

    Add new project

---
+++ For_While_If/If/Ostacha_Div4.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a % 4 == 0:
+    print(f"Ostacha {a} % 4 = 0")
+else:
+    if a % 4 == 1:
+        print(f"Ostacha {a} % 4 = 1")
+    else:
+        if a % 4 == 2:
+            print(f"Ostacha {a} % 4 = 2")
+        else:
+            print(f"Ostacha {a} % 4 = 3")
