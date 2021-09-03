327fc90c7c92fdc31e33b6d11bc03e1f801ec53f
Svitlana Pushchalo <svitlana8@gmail.com>
2021-09-03 22:38:50 +0300

    Add new project

--- Func/Goods_args_kwargs.py
+++ Func/Goods_args_kwargs.py
@@ -3,13 +3,12 @@ def print_goods(*args):
     for i in range(len(args)):
         if str(args[i]).isalpha() and (str(args[i]) not in ('True', 'False')):
             listt.append(args[i])
-        # elif str(args[i]).isdigit() and (str(args[i]) not in ('True', 'False')):
-        #     continue
     if len(listt) == 0:
         print("Нет товаров")
     else:
         for i in range(len(listt)):
             print(i + 1, '. ', listt[i], sep='')
+
 # print(print_goods('apple', 'banana', 'orange'))
 print(print_goods(1, True, 'Грушечка', '', 'Pineapple'))
 # print(print_goods([], {}, 1, 2))
@@ -29,9 +28,7 @@ print(print_goods(1, True, 'Грушечка', '', 'Pineapple'))
 #     else:
 #         for i in range(len(listt)):
 #             print(i + 1, '. ', listt[i], sep='')
-# print(print_goods('apple', 'banana', 'orange'))
-# print(print_goods(1, True, 'Грушечка', '', 'Pineapple'))
-# print(print_goods([], {}, 1, 2))
+
 
 
 # №3:
