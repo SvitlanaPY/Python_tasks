0754b00187092c4029f90270008240840cefd41e
Svitlana Pushchalo <svitlana8@gmail.com>
2021-09-05 15:16:39 +0300

    Add new project

--- Dict/Cities_Countries.py
+++ Dict/Cities_Countries.py
@@ -1,6 +1,7 @@
 # Задача «Страны и города»
 # Дан список стран и городов каждой страны. Затем даны названия городов.
 # Для каждого города укажите, в какой стране он находится.
+#
 # Inputs1:
 # 2
 # Russia Moscow Petersburg Novgorod Kaluga
@@ -20,6 +21,7 @@ for i in range(n):
     cc = input().split()
     key = cc[0]
     d[key] = cc[1:]
+print("Dict: ", d)   # Dict:  {'Russia': ['Moscow', 'Petersburg', 'Novgorod', 'Kaluga'], 'Ukraine': ['Kiev', 'Donetsk', 'Odessa']}
 m = int(input())
 for i in range(m):
     word = input()
