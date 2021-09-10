fed693c6c8120a2fac4e194d70b7fb2ec45f41e0
Svitlana Pushchalo <svitlana8@gmail.com>
2021-09-10 20:01:08 +0300

    Add new project

--- STRING_module.py
+++ STRING_module.py
@@ -3,12 +3,16 @@ print(printable)
 # 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 # 
 
+
 from string import ascii_uppercase
 print(ascii_uppercase)
 # ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
+
 from string import ascii_lowercase
 print(ascii_lowercase)
 # abcdefghijklmnopqrstuvwxyz
+
 ascii_l = ascii_lowercase
 print("ascii_lowercase: ", ascii_l)
+# ascii_lowercase:  abcdefghijklmnopqrstuvwxyz
