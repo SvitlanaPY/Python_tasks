1c1beea4b3702e6420522f3c2134989d4012ecf4
Svitlana Pushchalo <svitlana8@gmail.com>
2021-09-21 15:45:07 +0300

    Add new project

--- LOGging/UserOwn_Loggers.py
+++ LOGging/UserOwn_Loggers.py
@@ -1,18 +1,21 @@
 import logging
-# Create a custom logger
-my_logger = logging.getLogger(<name>)
 
-# Create handlers
+# Create a custom LOGGER:
+my_logger = logging.getLogger(__name__)
+
+# Create HANDLERs:
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler('file.log')
 
-# Set logs' level:
+# Set logs' level (for handlers'):
 c_handler.setLevel(logging.WARNING)
 f_handler.setLevel(logging.ERROR)
 
-# Create formatters and add it to handlers
+# Create FORMATTERs для хендлерів:
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add FORMATTERs to HANDLERs:
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 
@@ -27,3 +30,6 @@ my_logger.warning("warning msg")
 my_logger.error("error msg")
 my_logger.critical("critical msg")
 
+# ми створили ОДИН логер і два його хендлери: c_handler та f_handler
+# c_handler - виводить повідомлення рівня WARNING і вище (30-WARNING, 40-ERROR, та 50-CRITICAL) у консоль,
+# а f_handler - виводить повідомлення рівня ERROR та вище (40-ERROR, та 50-CRITICAL) у лог-файл 'file.log'
