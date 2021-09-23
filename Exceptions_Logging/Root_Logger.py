d027a3606c525c33fe56736689ea3d6b20a80cb4
Svitlana Pushchalo <svitlana8@gmail.com>
2021-09-23 22:35:47 +0300

    Add new project

--- Exceptions_Logging/Root_Logger.py
+++
@@ -1,3 +0,0 @@
-
-# Template_format = "%(asctime)s: %(name)s: %(levelnames)s - %(message)s"
-# logging.basicConfig(level=logging.INFO, filename="Calculator_Logs.log", filemode="a", format=Template_format)
