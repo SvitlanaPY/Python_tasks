71949637f8944f9f870f8a7bfc66cb08082eda72
Svitlana Pushchalo <svitlana8@gmail.com>
2021-10-01 16:10:40 +0300

    changes

--- OOP/Point.py
+++ OOP/Point.py
@@ -1,3 +1,22 @@
+"""
+Создайте класс Point. У этого класса должны быть
+
+метод set_coordinates, который принимает координаты по x и по y,
+и сохраняет их в экземпляр класса соответственно в атрибуты x и y
+метод get_distance, который обязательно принимает экземпляр класса Point
+и возвращает расстояние между двумя точками по теореме Пифагора.
+В случае, если в данный метод передается не экземпляр класса Point необходимо вывести сообщение "Передана не точка"
+Пример работы с классом Point
+
+p1 = Point()
+p2 = Point()
+p1.set_coordinates(1, 2)
+p2.set_coordinates(4, 6)
+d = p1.get_distance(p2) # вернёт 5.0
+p1.get_distance(10) # Распечатает "Передана не точка"
+"""
+
+
 class Point:
     def set_coordinates(self, x, y):
         self.x = x
@@ -9,3 +28,12 @@ class Point:
         else:
             print(f"Передана НЕ точка")
 
+
+p1 = Point()
+p2 = Point()
+p1.set_coordinates(1, 2)
+p2.set_coordinates(4, 6)
+d = p1.get_distance(p2)  # вернёт 5.0
+print("d=", d)
+dd = p1.get_distance(10)
+print("dd=", dd)  # Распечатает "Передана не точка"
