8321ce3534f25d23f83e359a516c0ec5d770588c
Svitlana Pushchalo <svitlana8@gmail.com>
2022-01-22 01:28:01 +0200

    change FizzBuzz

--- For_While_If/If/FizzBuzz.py
+++ For_While_If/If/FizzBuzz.py
@@ -15,3 +15,14 @@ elif n % 3 == 0:
         print('Fizz')
 else:
     print('Buzz')
+
+
+# a=int(input())
+# if a%3==0 and a%5==0:
+#     print('FizzBuzz')
+# elif a%3==0:
+#     print('Fizz')
+# elif a%5==0:
+#     print('Buzz')
+# else:
+#     print(a)
