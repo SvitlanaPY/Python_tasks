00813c73b3d2f20c51954669ff05ac9306d3fd0a
Svitlana Pushchalo <svitlana8@gmail.com>
2022-06-20 00:49:39 +0300

    "changes_20June2022"

--- For_While_If/If/FizzBuzz.py
+++ For_While_If/If/FizzBuzz.py
@@ -1,28 +1,31 @@
-# FizzBuzz
-# Ваша программа должна считать одно натуральное число, после чего вывести:
-# “Fizz”, если это число делится на 3;
-# “Buzz”, если это число делится на 5;
-# “FizzBuzz”, если выполнены оба предыдущих условия;
-# само это число в остальных случаях.
+"""
+FizzBuzz
+Ваша программа должна считать одно натуральное число, после чего вывести:
+“Fizz”, если это число делится на 3;
+“Buzz”, если это число делится на 5;
+“FizzBuzz”, если выполнены оба предыдущих условия;
+само это число в остальных случаях.
+"""
 
 n = int(input())
-if n % 5 != 0 and n % 3 != 0:
-    print(n)
-elif n % 3 == 0:
+if n % 3 == 0:
     if n % 5 == 0:
         print('FizzBuzz')
     else:
         print('Fizz')
-else:
+elif n % 5 == 0:
     print('Buzz')
+else:
+    print(n)
 
 
-# a=int(input())
-# if a%3==0 and a%5==0:
-#     print('FizzBuzz')
-# elif a%3==0:
-#     print('Fizz')
-# elif a%5==0:
-#     print('Buzz')
+# n = int(input())
+# if n % 5 != 0 and n % 3 != 0:
+#     print(n)
+# elif n % 3 == 0:
+#     if n % 5 == 0:
+#         print('FizzBuzz')
+#     else:
+#         print('Fizz')
 # else:
-#     print(a)
+#     print('Buzz')
--- For_While_If/If/Ostacha_Div4.py
+++ For_While_If/If/Ostacha_Div4.py
@@ -1,12 +1,21 @@
-a = int(input())
+# a = int(input())
+# if a % 4 == 0:
+#     print(f"Ostacha {a} % 4 = 0")
+# else:
+#     if a % 4 == 1:
+#         print(f"Ostacha {a} % 4 = 1")
+#     else:
+#         if a % 4 == 2:
+#             print(f"Ostacha {a} % 4 = 2")
+#         else:
+#             print(f"Ostacha {a} % 4 = 3")
 
+a = int(input())
 if a % 4 == 0:
     print(f"Ostacha {a} % 4 = 0")
-else:
-    if a % 4 == 1:
-        print(f"Ostacha {a} % 4 = 1")
-    else:
-        if a % 4 == 2:
-            print(f"Ostacha {a} % 4 = 2")
-        else:
-            print(f"Ostacha {a} % 4 = 3")
+elif a % 4 == 1:
+    print(f"Ostacha {a} % 4 = 1")
+elif a % 4 == 2:
+    print(f"Ostacha {a} % 4 = 2")
+elif a % 4 == 3:
+    print(f"Ostacha {a} % 4 = 3")
