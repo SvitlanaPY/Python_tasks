56d03406a97bd70709aa5ff630b25de9dfc2acce
Svitlana Pushchalo <svitlana8@gmail.com>
2022-06-30 18:25:58 +0300

    "changes_30June2022"

---
+++ For_While_If/VkladeniTsykly/Drabynka.py
@@ -0,0 +1,25 @@
+"""
+Задача «Лесенка»
+По данному натуральному n ≤ 9 выведите лесенку из n ступенек,
+i-я ступенька состоит из чисел от 1 до i без пробелов.
+"""
+
+# n = int(input())
+# x = 0
+# for i in range(1, n+1):
+#     x = x * 10 + i
+#     print(x)
+
+
+n = int(input())
+x = ''
+for i in range(0, n):
+    x = x + str(i + 1) + ' '
+    print(x)
+
+
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(i):
+        print(j + 1, end=' ')
+    print()
