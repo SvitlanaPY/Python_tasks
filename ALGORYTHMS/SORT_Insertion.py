7421695d73ffd29a5e856bc6e86b42932852a597
Svitlana Pushchalo <svitlana8@gmail.com>
2022-07-01 15:56:12 +0300

    "changes_01July2022"

--- ALGORYTHMS/SORT_Insertion.py
+++ ALGORYTHMS/SORT_Insertion.py
@@ -20,30 +20,17 @@ Sample Input 2:
 Sample Output 2:
 1 2 5 8 11
 """
-count = 0
-n = 10
-mas = [12, 14, 5, 7, 4, 3, 8, -15, 2, -7]
+
+n = 10    # n = int(input())
+mas = [12, 14, 5, 7, 4, 3, 8, -15, 2, -7]    # mas = list(map(int, input().split()))
+#index: 0   1  2  3  4  5  6    7   8   9
+count = 0    # кількість перестановок
 for i in range(1, n):
     save = mas[i]    # у змінну save записуємо елемент для вставки; індекс елемента = № обходу; save - як підмасив
-    while i != 0 and mas[i - 1] > save:   # порвняння елементу, записаного в save із попереднім елементом
+    while i != 0 and mas[i - 1] > save:   # порівняння елементу, записаного в змінну save із попереднім елементом
         count += 1
         mas[i] = mas[i - 1]   # переміщення/зсув вправо елемента, більшого за save
         i -= 1   # зміщення циклу вліво (зменшення)
     mas[i] = save   # вставляємо менший елемент у потрібну позицію
-print(mas)
+print(*mas)
 print(count)
-
-
-# count = 0
-# n = 10
-# mas = [12, -4, 5, 7, 4, 3, 8, -15, 2, -7]
-# for i in range(1, n):
-#     save = mas[i]    # у змінну save записуємо елемент для вставки; індекс елемента = № обходу
-#     j = i
-#     while j != 0 and mas[j - 1] > save:
-#         count += 1
-#         mas[j] = mas[j - 1]
-#         j -= 1
-#     mas[j] = save
-# print(mas)
-# print(count)
